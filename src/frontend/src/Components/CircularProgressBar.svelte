778167a6c4c8d1beb4a71570eb205a13be840054
Ryan Zmuda <ryanzmuda@gmail.com>
2022-08-20 17:11:22 -0400

    display some server stats and catch (some) errors

---
+++ src/frontend/src/Components/CircularProgressBar.svelte
@@ -0,0 +1,26 @@
+<!-- YOINKED FROM: -->
+<!-- https://svelte.dev/repl/f1437286b08d4890b9207180868ee37e?version=3.46.4 -->
+<script>
+	export let progress
+	const angle = 360 * progress
+	
+	// Adapt the logic according to the approach
+	const	background = `radial-gradient(white 50%, transparent 51%),
+    conic-gradient(transparent 0deg ${angle}deg, gainsboro ${angle}deg 360deg),
+    conic-gradient(orange 0deg, yellow 90deg, lightgreen 180deg, green);`;
+	
+	$: cssVarStyles = `--background:${background}`
+</script>
+
+<style>
+#progress-circle {
+  background: var(--background);
+  border-radius: 50%;
+  width: 120px;
+  height: 120px;
+	transition: all 500ms ease-in;
+	will-change: transform;
+}
+</style>
+
+<div id="progress-circle" style="{cssVarStyles}"></div>
--- src/frontend/src/Components/ServerStats.svelte
+++ src/frontend/src/Components/ServerStats.svelte
@@ -1,7 +1,10 @@
 <script>
     import App from "../App.svelte";
     import axios from 'axios';
+    import CircularProgressBar from "./CircularProgressBar.svelte";
+
     var loaded = false;
+    var error = true;
     var serverdata;
     var uptime_stamp;
 
@@ -9,13 +12,15 @@
         axios.get('server_address/api/getstats')
         .then(function (response) {
             serverdata = response.data;
+            error = false;
             loaded = true;
             var uptime_seconds = Math.round(new Date() / 1000) - serverdata['boot-time'];
             uptime_stamp = (Math.floor(uptime_seconds/86400) + ":" + (new Date(uptime_seconds * 1000)).toISOString().substr(11, 8)).split(":");
             
         })
         .catch(function (error) {
-            console.log(error);
+            error = true;
+            loaded = true;
         });
         setTimeout(getstats,1000) //TODO LOWER THIS FOR PROD BUT 1 SECOND LOOKS COOLER
     }
@@ -29,10 +34,32 @@
     {#if !loaded}
         <div class="spinner"></div>
     {:else}
-        <div class="text-fit">
-            <h1 class="text-fit">Server: {serverdata['ip']}</h1>
-            <p class="text-fit">Up For: {(`${uptime_stamp[0]} days, ${uptime_stamp[1]} hours, ${uptime_stamp[2]} minutes, ${uptime_stamp[3]} seconds` )}</p>
-        </div>
+        {#if error}
+            <div class="error-fit">
+                <h1 style="color: red;">Server is offline</h1>
+                <p style="color: gray;">If you are seeing this message, something is seriously wrong ;-;</p>
+            </div>
+        {:else}
+
+        <!--PUT A CASE FOR ERROR HERE-->
+            <div class="internal-fit">
+                <h1 class="text-fit">Server: {serverdata['ip']}</h1>
+                <p class="text-fit">Up For: {(`${uptime_stamp[0]}d ${uptime_stamp[1]}h ${uptime_stamp[2]}m ${uptime_stamp[3]}s` )}</p>
+
+                <progress class="pbar" max="100" value={serverdata['load-average']}/>
+                <p class="text-fit">Load Average: {serverdata['load-average']}%</p>
+                <!-- <CircularProgressBar progress={serverdata['disk']['percent'] / 100}/> -->
+                <progress class="pbar" max="100" value={serverdata['disk']['percent']}/>
+                <p class="text-fit">Disk Usage: {serverdata['disk']['used']}GB/{serverdata['disk']['total']}GB</p>
+                <!-- <CircularProgressBar progress={serverdata['memory']['percent'] / 100}/> -->
+                <progress class="pbar" max="100" value={serverdata['memory']['percent']}/>
+                <p class="text-fit">RAM Usage: {serverdata['memory']['used']}GB/{serverdata['memory']['total']}GB</p>
+                <!-- <CircularProgressBar progress={serverdata['swap']['percent'] / 100}/> -->
+                <progress class="pbar" max="100" value={serverdata['swap']['percent']}/>
+                <p class="text-fit">Swap Usage: {serverdata['swap']['used']}GB/{serverdata['swap']['total']}GB</p>
+                <!-- <CircularProgressBar progress={serverdata['load-average'] / 100}/> -->
+            </div>
+        {/if}
     {/if}
 </div>
 
@@ -63,22 +90,27 @@
             transform: rotate(1turn);
         }
     }
-    @media screen and (min-width: 700px) {
-
-    .text-fit {
-
-    font-size: 25px;
-
+    .internal-fit {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: left;
     }
-
+    .text-fit{
+        margin: 0px;
     }
-    @media screen and (max-width: 430px) {
-
-    .text-fit {
-
-    font-size: 15px;
-
+    .pbar{
+        width: 90%;
+        margin: 10px;
     }
-
-}
+    .error-fit{
+        width: 100%;
+        height: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+        text-align: center;
+    }
 </style>
